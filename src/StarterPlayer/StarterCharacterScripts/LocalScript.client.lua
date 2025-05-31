84a317c2b9597fee87b2dbe8b1417c5dd979397b
skidded <131218588+dukapanzer@users.noreply.github.com>
2025-06-01 00:02:08 +0300

    Update LocalScript.client.lua
    small optimization added

--- src/StarterPlayer/StarterCharacterScripts/LocalScript.client.lua
+++ src/StarterPlayer/StarterCharacterScripts/LocalScript.client.lua
@@ -1,4 +1,22 @@
-local player = game.Players.LocalPlayer
-player:GetMouse().Button1Down:Connect(function()
-	game.ReplicatedStorage.RemoteEvent:FireServer()
+local player = game:GetService("Players").LocalPlayer
+local mouse = player:GetMouse()
+local mouse_held = false
+local hb = game:GetService("RunService").Heartbeat
+
+-- // hold click to attack \\ --
+-- || made by: @dukapanzer || --
+mouse.Button1Down:Connect(function()
+	mouse_held = true
 end)
+
+mouse.Button1Up:Connect(function()
+	mouse_held = false
+end)
+
+hb:Connect(function()
+	if mouse_held then
+		game.ReplicatedStorage.RemoteEvent:FireServer()
+	end
+end)
+-- || made by: @dukapanzer || --
+-- \\ hold click to attack // --
